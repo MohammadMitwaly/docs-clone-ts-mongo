3d6e1c252e8f89322a0ef39957719464a8e81fdb
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-05-10 20:10:06 +0300

    Setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,20 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 
 function App() {
   return (
-    <div>
-      <Editor />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <Editor />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
