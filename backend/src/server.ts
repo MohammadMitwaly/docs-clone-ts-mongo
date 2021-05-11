70c25313d6894a12b1294d8c7fdcf2dc4742d29a
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-05-11 12:39:10 +0300

    Add DB functionality to BE

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,13 +18,20 @@ const IO = new SocketIO(5000, {
 });
 
 IO.on("connection", (socket) => {
-  socket.on("get-document", (documentID) => {
-    const tempData = "";
+  socket.on("get-document", async (documentID) => {
+    const userDocument = await findOrCreateDoc(documentID);
     socket.join(documentID);
-    socket.emit("load-document", tempData);
+    socket.emit(
+      "load-document",
+      userDocument && userDocument["data"] ? userDocument["data"] : ""
+    );
 
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentID).emit("receive-changes", delta);
+    });
+
+    socket.on("save-changes", async (data) => {
+      await saveDocument(documentID, data);
     });
   });
 });
@@ -36,4 +43,8 @@ const findOrCreateDoc = async (docId: string) => {
   const userDocument = await Document.findById(docId);
   // If the document exists, return it, else create a new one based on the passed in ID
   return userDocument || (await Document.create({ _id: docId, data: "" }));
+};
+
+const saveDocument = async (docId: string, data) => {
+  await Document.findByIdAndUpdate(docId, { data });
 };
